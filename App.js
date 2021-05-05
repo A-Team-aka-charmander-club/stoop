5bd2858b3b165e6c9e44b04320fc7f08a99ee12e
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-05-05 12:13:12 -0400

    Create react native app

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import logo from './assets/logo.png'
+import * as ImagaPicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagaPicker.requestMediaLibraryPermissionsAsync();
+
+    if(!permissionResult.granted){
+      alert("Permission to acccess camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult, 'picker result')
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Image source={logo} style={styles.image}/>
+      <Text style={styles.text}>To share photo with a friend, just press the button</Text>
+      {/* <StatusBar style="auto" />
+       */}
+       <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}
+       >
+         <Text style={styles.buttonText}>
+           Take a photoo
+         </Text>
+       </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +43,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15
+  },
+  image: {
+    width: 305,
+    height: 159,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor:'blue',
+    padding: 20,
+    borderRadius: 5
+  }, 
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  }
 });
