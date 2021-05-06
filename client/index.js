db3a69d2b5979d8f0ab8053ad7d12b4a5aefc6ab
Anna Strasser <albastrasser@gmail.com>
2021-05-06 17:48:44 -0400

    added firebase and attempting login functionality

---
+++ client/index.js
@@ -0,0 +1,5 @@
+export { default as LoginScreen } from './component/LoginScreen/LoginScreen';
+
+export { default as HomeScreen } from './component/HomeScreen/HomeScreen';
+
+export { default as RegistrationScreen } from './component/RegistrationScreen/RegistrationScreen';
