8ddc3eff3e152270413c61ee74b7d5d04cff7214
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-05-13 12:20:17 -0400

    Update: user model and route

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,13 +2,23 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
-  firebaseId: {
+  firebaseUserId: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
       notEmpty: true,
     },
   },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }, 
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
 module.exports = User;
