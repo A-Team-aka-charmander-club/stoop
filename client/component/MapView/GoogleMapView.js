683ae900d21ec94706ae6eaad7ec4634f5771b53
Allie Humphrey <alliehumphrey@yahoo.com>
2021-05-13 21:01:34 -0500

    Still working on geolocation, switching to expo location

--- client/component/MapView/GoogleMapView.js
+++ client/component/MapView/GoogleMapView.js
@@ -3,31 +3,37 @@ import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { Text, View } from 'react-native';
 import styles from './styles';
 import Geolocation from 'react-native-geolocation-service';
+import * as Location from 'expo-location';
 
 export default function GoogleMapView() {
-  const [latitude, setLatitude] = React.useState(0);
-  const [longitude, setLongitude] = React.useState(0);
-  const [coordinates, setCoordinates] = React.useState([]);
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-        setCoordinates(
-          coordinates.concat({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          })
-        );
-      },
-      {
-        showLocationDialog: true,
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 0,
-      }
-    );
-  });
+  // const [latitude, setLatitude] = React.useState(0);
+  // const [longitude, setLongitude] = React.useState(0);
+  // const [coordinates, setCoordinates] = React.useState([]);
+  // useEffect(() => {
+  //   if (hasLocationPermission) {
+  //     Geolocation.getCurrentPosition(
+  //       (position) => {
+  //         setLatitude(position.coords.latitude);
+  //         setLongitude(position.coords.longitude);
+  //         setCoordinates(
+  //           coordinates.concat({
+  //             latitude: position.coords.latitude,
+  //             longitude: position.coords.longitude,
+  //           })
+  //         );
+  //       },
+  //       (error) => {
+  //         console.log(error.code, error.message);
+  //       },
+  //       {
+  //         showLocationDialog: true,
+  //         enableHighAccuracy: true,
+  //         timeout: 20000,
+  //         maximumAge: 0,
+  //       }
+  //     );
+  //   }
+  // });
   return (
     <View style={styles.container}>
       <MapView
