03d3876349b4da4d07a0c47ca2c8fcb7cbade210
Abi Scholz <abi.scholz@gmail.com>
2021-05-19 16:00:33 -0400

    Fix: merging all changes

--- client/component/Navigation/StackNavigator/StackNavigator.js
+++ client/component/Navigation/StackNavigator/StackNavigator.js
@@ -12,9 +12,9 @@ import EditPostScreen from '../../PostScreen/EditPostScreen/EditPostScreen';
 const Stack = createStackNavigator();
 const HomeStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName='Home'>
       <Stack.Screen
-        name="Home"
+        name='Home'
         component={HomeScreen}
         options={{ headerShown: false }}
       />
@@ -41,25 +41,21 @@ const HomeStackNavigator = () => {
 
 const PostStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Post">
+    <Stack.Navigator initialRouteName='Post'>
       <Stack.Screen
-        name="Post"
+        name='Post'
         component={PostScreen}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="SinglePost"
+        name='SinglePost'
         component={SinglePost}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-<<<<<<< HEAD
         name='Comments'
         component={Comments}
         name='Edit'
-=======
-        name="Edit"
->>>>>>> editDeletePost
         component={EditPostScreen}
         options={{ headerShown: false }}
       />
@@ -69,9 +65,9 @@ const PostStackNavigator = () => {
 
 const MapStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Map">
+    <Stack.Navigator initialRouteName='Map'>
       <Stack.Screen
-        name="Map"
+        name='Map'
         component={GoogleMapView}
         options={{ headerShown: false }}
       />
