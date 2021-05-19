85abe7987967d91cecfa8afd0576fd7a91d05e47
Abi Scholz <abi.scholz@gmail.com>
2021-05-19 11:13:07 -0400

    testing testing

--- client/store/post.js
+++ client/store/post.js
@@ -48,7 +48,7 @@ export const createPostThunk = (post) => {
       console.log('IN POST THUNK');
       const user = firebase.auth().currentUser;
       const { data } = await axios.post(
-        `http://192.168.1.152:8080/api/posts/post`,
+        `http://localhost:8080/api/posts/post`,
 
         post,
         {
