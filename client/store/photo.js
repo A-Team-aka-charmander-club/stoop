5aef526644b26af3d4bf8ddccd865d4335adf875
Allie Humphrey <alliehumphrey@yahoo.com>
2021-05-19 19:57:34 -0500

    fixing merge conflicts

--- client/store/photo.js
+++ client/store/photo.js
@@ -40,7 +40,7 @@ export const addPhotoThunk = (firebasePhotoId, photoUrl) => {
         // 'http://localhost:8080/api/photos/photo',
         //'http://192.168.1.152:8080/api/photos/photo',
         //anna's ip address: 192.168.1.152
-        `http://192.168.1.6:8080/api/photos/photo`,
+        `http://10.0.0.153:8080/api/photos/photo`,
         {
           firebasePhotoId,
           photoUrl,
