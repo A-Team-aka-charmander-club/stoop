f0ef348c778f2fdd75c5eb5f15eeff0cb8c04350
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-05-21 14:59:04 -0400

    Weird error

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,6 +12,7 @@ import '@firebase/auth';
 import '@firebase/firestore';
 import '@firebase/storage';
 console.log('API KEY', REACT_APP_API_KEY);
+console.log('API KEY', REACT_APP_API_KEY);
 const firebaseConfig = {
   apiKey: REACT_APP_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
